063ffefe70bb4660c26a1e587f4641c17805b616
hiromeister <hermandrobin@gmail.com>
2017-12-22 01:46:04 +0400

    go heroku

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
+const dotEnv = require('dotenv').load();
+
 module.exports = {
 
-    'url' : 'mongodb://localhost:27017/cybercafe' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    // 'url' : 'mongodb://localhost:27017/cybercafe' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
      
      //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
+
+     'url': process.env.MONGODB_URI
 };
